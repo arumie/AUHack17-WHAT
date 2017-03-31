c673e899ff78ad77167cc0928fbb69605f885113
Jonas Tranberg <jonas_ts@msn.com>
2017-03-31 22:07:18 +0200

    init app

---
+++ app.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var fs = require('fs');
+var https = require('https');
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+var options = {
+    key: fs.readFileSync( path.join(__dirname, 'keys/server.key') ),
+    cert: fs.readFileSync( path.join(__dirname, 'keys/server.crt') )
+};
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+var server = https.createServer(options, app).listen(port, function(){
+	console.log("Express server listening on port " + port);
+});
+
+app.get('/', function(req, res){
+	res.send('Hello World');
+});
