d759b76cbdb78d7f9e4c3a51e6289baa4d19a225
Ulrik Boll Djurtoft <Ullebe1@gmail.com>
2017-04-01 02:21:50 +0200

    Added class for playing sounds

---
+++ public/js/audio.js
@@ -0,0 +1,22 @@
+class Audio {
+
+  error(){
+    var audio = new Audio('../assets/error.mp3');
+    audio.play();
+  }
+
+  entering(){
+    var audio = new Audio('../assets/entering.mp3');
+    audio.play();
+  }
+
+  leaving(){
+    var audio = new Audio('../assets/leaving.mp3');
+    audio.play();
+  }
+
+  ringtone(){
+    var audio = new Audio('../assets/ringetone.mp3');
+    audio.play();
+  }
+}
