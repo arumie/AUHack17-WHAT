758ab86be221a51ce6f52c5c32a35f790c79941e
Jonas Tranberg <jonas_ts@msn.com>
2017-04-01 02:10:39 +0200

    Added loading screen

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,13 @@ class Main {
 	constructor() {
 		this.voiceChat = new VoiceChat();
 		this.geolocation = new GeoService();
+
+		setTimeout(this.removeInitializationScreen, 4000);
 	}
 
-	
+	removeInitializationScreen() {
+		document.getElementById("loading-screen").style = "display: none";
+	}
 }
 
 //init voicechat when dom is loaded
