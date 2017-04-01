caeddf1ea70783e70040bf364ffe5b73667cfb29
arumie <david.zachariae@gmail.com>
2017-04-01 02:41:48 +0200

    doStuff in init

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,27 @@
 class Main {
 	constructor() {
+		const self = this;
+		this.doStuffElem = document.getElementById("do-stuff");
 		this.voiceChat = new VoiceChat();
 		this.geolocation = new GeoService();
-
+		this.doStuffArr = ["Stacking crates...", "Feeding the pet rock...", "Brewing coffee...", "Giving the browser a motivational speech...", "Having a blast...", "Deleting random stuff...", "Eating chips...", "Eating shit...", "WHAT...!? WHERE...!?", "Looking for a girlfriend..."];
 		setTimeout(this.removeInitializationScreen, 4000);
+		this.doStuffElem.innerHTML = 'Welcome!'
+		setInterval(function(){
+			var elem = document.getElementById("do-stuff");
+			var randomNr =   self.getRandomInt(0,self.doStuffArr.length-1);
+			self.doStuffElem.innerHTML = self.doStuffArr[randomNr];
+		}, 1000);
 	}
 
 	removeInitializationScreen() {
 		document.getElementById("loading-screen").style = "display: none";
 		document.getElementById("main-scene").style = "display: block";
+	}
+
+
+	getRandomInt(min, max) {
+  		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 }
 
